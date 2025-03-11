c71363e8b50e9e6f132268ca40747a3cb84e52de
Soroush Keyana <soroush.keyana@gmail.com>
2025-03-11 11:09:54 +0100

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,8 +32,12 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://diagonalley-efdn.onrender.com']
 
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://diagonalley-efdn.onrender.com',
+]
 
 # Application definition
 
